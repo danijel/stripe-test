6426175892c9f32d89ea4d8c92105c469000bea4
danijel <danijel.bjelajac@gmail.com>
2019-02-28 13:52:37 +0100

    first commit
    only did
    expo init
    expo eject
    took App.js, src from https://github.com/expo/expo/tree/master/packages/expo-payments-stripe/examples/with-expokit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expf2ef3220ef4841dca34c0c0482e88bb5://10.0.1.2:19000";
+
+}
